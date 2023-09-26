0e631358c2586f0a27edcb9cbee58f4714e40983
Naod <naodk36@gmail.com>
2023-09-26 13:43:55 -0700

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
  * print_listint - print all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
- * Return: no of nodess
+ * Return: num of nodes
  */
 size_t print_listint(const listint_t *h)
 {
@@ -19,3 +19,4 @@ size_t print_listint(const listint_t *h)
 
 	return (num);
 }
+
